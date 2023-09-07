f9f00e72b416110e518f2a317358613cb294b9bd
Kiren <kirenraj21m@forwardschool.edu.my>
2023-09-07 16:40:43 +0800

    Updated Fragment,ViewModel, API, etc

---
+++ app/src/main/java/com/justin/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.justin
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.justin.newsapplication.R
+
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+}
