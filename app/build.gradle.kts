8c8324988f73dd28f7223876ecf5bd143c5ab323
justin <justinchiengzeekhai@gmail.com>
2023-09-07 10:03:59 +0800

    added placeholder bbc.jpeg edited and added implementation from build.gradle.kts added colors.xml Created DetailsFragment.kt and fragment_details.xml created vector images ic_img.xml and ic_share.xml created navgraph.xml and added fragments to include args
    Currently awaiting teammates'side code for implemention of binding show newsdetails text in DetailsFragment.kt (waiting for repo file , to include in detailsviewmodel to use val Factory)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -62,4 +62,6 @@ dependencies {
     implementation("androidx.room:room-ktx:2.5.2")
     implementation ("com.squareup.retrofit2:retrofit:2.9.0")
     implementation ("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation ("com.github.bumptech.glide:glide:4.16.0")
+    annotationProcessor("com.github.bumptech.glide:glide:4.16.0")
 }
