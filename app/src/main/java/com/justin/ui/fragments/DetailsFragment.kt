3cc72aae864656efb9e64456763ac0b22b5ddc32
Kiren <kirenraj21m@forwardschool.edu.my>
2023-09-15 14:46:56 +0800

    Updated Fragment,ViewModel, API, etc

--- app/src/main/java/com/justin/ui/fragments/DetailsFragment.kt
+++ app/src/main/java/com/justin/ui/fragments/DetailsFragment.kt
@@ -4,17 +4,28 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import androidx.databinding.DataBindingUtil.setContentView
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import com.bumptech.glide.Glide
+import com.justin.data.model.NewsModel
+import com.justin.data.retrofit.NewsApi
 import com.justin.newsapplication.R
 import com.justin.newsapplication.databinding.FragmentDetailsBinding
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 class DetailsFragment : Fragment() {
 
     private lateinit var binding: FragmentDetailsBinding
     private val args: DetailsFragmentArgs by navArgs()
+
+    private lateinit var apiService: NewsApi
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -22,6 +33,34 @@ class DetailsFragment : Fragment() {
     ): View? {
         binding = FragmentDetailsBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val retrofit = Retrofit.Builder()
+            .baseUrl("https://newsapi.org/v2/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+
+        apiService = retrofit.create(NewsApi::class.java)
+
+        val call = apiService.getNews()
+        call.enqueue(object : Callback<NewsModel> {
+            override fun onResponse(call: Call<NewsModel>, response: Response<NewsModel>) {
+                if (response.isSuccessful) {
+                    val apiResponse = response.body()
+                    val textContent = apiResponse?.content ?: "No content available"
+                    val tvNews = findViewById<TextView>(R.id.tvNews)
+                    tvNews.text = textContent
+                }
+            }
+
+            override fun onFailure(call: Call<NewsModel>, t: Throwable) {
+                TODO("Not yet implemented")
+            }
+
+        })
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -47,4 +86,3 @@ class DetailsFragment : Fragment() {
     }
 }
 
-}
